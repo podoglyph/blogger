6fe0440c4cc8a2d4177f09b42ef8671157b7dc90
Padraic O'Neill <pconeill@gmail.com [core]>
2017-05-15 20:51:57 -0600

    I2, the third time

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' Annihilated!"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article
+    flash.notice = "Article '#{@article.title}' Edited!"
+  end
+
+  def update
+  @article.update(article_params)
+
+  flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
